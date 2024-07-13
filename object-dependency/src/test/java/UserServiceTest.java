dc5858be4c735ad35b33d48b5b804b42176c9c8c
데이터본부-개발팀-송평현 <ph.song@ust21.co.kr>
2024-07-13 16:51:32 +0900

    6.8.2 Transaction 동기화 Test transactionSync 추가

--- object-dependency/src/test/java/UserServiceTest.java
+++ object-dependency/src/test/java/UserServiceTest.java
@@ -66,6 +66,13 @@ public class UserServiceTest {
 
   }
 
+  @Test
+  public void transactionSync() {
+    userService.deleteAll();
+    userService.add(users.get(0));
+    userService.add(users.get(1));
+  }
+
   @Test()
   public void readOnlyTransactionAttribute() {
     assertThrows(TransientDataAccessResourceException.class, () -> testUserService.getAll());
